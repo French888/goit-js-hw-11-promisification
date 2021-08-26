e3ffa2c5e94c4aa34c382c4a17d6613be7e6fb02
Oleg Bloschinskyi <carrentkharkov@gmail.com>
2021-08-27 02:57:59 +0300

    ok

--- index.js
+++ index.js
@@ -16,30 +16,73 @@
 //   delay(1500).then(logger); // Resolved after 1500ms
   
 //   #TASK:2
-const users = [
-    { name: 'Mango', active: true },
-    { name: 'Poly', active: false },
-    { name: 'Ajax', active: true },
-    { name: 'Lux', active: false },
-  ];
+// const users = [
+//     { name: 'Mango', active: true },
+//     { name: 'Poly', active: false },
+//     { name: 'Ajax', active: true },
+//     { name: 'Lux', active: false },
+//   ];
   
-//   const toggleUserState = (allUsers, userName) => {
-//       return new Promise((resolve)=>{
-//         const updatedUsers = allUsers.map(user =>
-//             user.name === userName ? { ...user, active: !user.active } : user
-//         )
-//     resolve(updatedUsers)
-//         }); 
-//   };
-const toggleUserState = (allUsers, userName) => {
-      const updatedUsers = allUsers.map(user =>
-          user.name === userName ? { ...user, active: !user.active } : user
-      )
-  return Promise.resolve(updatedUsers)
-      }; 
+// //   const toggleUserState = (allUsers, userName) => {
+// //       return new Promise((resolve)=>{
+// //         const updatedUsers = allUsers.map(user =>
+// //             user.name === userName ? { ...user, active: !user.active } : user
+// //         )
+// //     resolve(updatedUsers)
+// //         }); 
+// //   };
+// const toggleUserState = (allUsers, userName) => {
+//       const updatedUsers = allUsers.map(user =>
+//           user.name === userName ? { ...user, active: !user.active } : user
+//       )
+//   return Promise.resolve(updatedUsers)
+//       }; 
 
-  const logger = updatedUsers => console.table(updatedUsers);
+//   const logger = updatedUsers => console.table(updatedUsers);
   
-  toggleUserState(users, 'Mango').then(logger);
-  toggleUserState(users, 'Lux').then(logger);
+//   toggleUserState(users, 'Mango').then(logger);
+//   toggleUserState(users, 'Lux').then(logger);
+// #TASK 3
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+  
+  const makeTransaction = (transaction) => {
+    const delay = randomIntegerFromInterval(200, 500);
+    return new Promise((resolve, reject)=>{
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
+    if (canProcess) {
+        resolve({id: transaction.id, time: delay});
+      } else {
+        reject(transaction.id);
+      }
+    }, delay);
+  });
+};    
+  
+  const logSuccess = ({id, time}) => {
+    console.log(`Transaction ${id} processed in ${time}ms`);
+  };
+  
+  const logError = id => {
+    console.warn(`Error processing transaction ${id}. Please try again later.`);
+  };
+  
+  makeTransaction({ id: 70, amount: 150 })
+    .then(logSuccess)
+    .catch(logError);
+  
+  makeTransaction({ id: 71, amount: 230 })
+    .then(logSuccess)
+    .catch(logError);
+  
+  makeTransaction({ id: 72, amount: 75 })
+    .then(logSuccess)
+    .catch(logError);
+  
+  makeTransaction({ id: 73, amount: 100 })
+    .then(logSuccess)
+    .catch(logError);
   
